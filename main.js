c1668044e6c2f536eeebd0ac699daeb744e002e4
Sobirjon <sobirjoneshniyozov@gmil.com>
2021-07-07 20:16:16 +0500

    Chat panel completed

--- main.js
+++ main.js
@@ -1,65 +1,28 @@
 
 $(document).ready(function () {
-
-    // $("#chatButton_1").click(function() {
-    //     if (this) {
-    //         $("#chatButton_1").addClass("active_chat");
-    //         $("#chatButton1").show();
-    //     } else {
-    //         $("#chatButton_1").removeClass("active_chat");
-    //         $("#chatButton1").hide();
-    //     }
-    // });
-    //  $("#chatButton_2").click(function() {
-    //     if (this) {
-    //         $("#chatButton_2").addClass("active_chat");
-    //         $("#chatButton2").show();
-    //     } else {
-    //         $("#chatButton_2").removeClass("active_chat");
-    //         $("#chatButton2").hide();
-    //     }
-    // });
-    //  $("#chatButton_3").click(function() {
-    //     if (this) {
-    //         $("#chatButton_3").addClass("active_chat");
-    //         $("#chatButton3").show();
-    //     } else {
-    //         $("#chatButton_3").removeClass("active_chat");
-    //         $("#chatButton3").hide();
-    //     }
-    // });
-    //  $("#chatButton_4").click(function() {
-    //     if (this == "") {
-    //         $("#chatButton_4").addClass("active_chat");
-    //         $("#chatButton4").show();
-    //     } else {
-    //         $("#chatButton_4").removeClass("active_chat");
-    //         $("#chatButton4").hide();
-    //     }
-    // });
-     $("body").click(function(e) {
-        if (e.target.id == "chatButton_1") {
+     $(".leftPanel").click(function(e) {
+        if (e.target.id == "chatButton_1" || $(e.target).parents("#chatButton_1").length) {
             $("#chatButton_1").addClass("active_chat");
             $("#chatButton1").show();
         } else {
             $("#chatButton_1").removeClass("active_chat");
             $("#chatButton1").hide();
         }
-        if (e.target.id == "chatButton_2") {
+        if (e.target.id == "chatButton_2" || $(e.target).parents("#chatButton_2").length) {
             $("#chatButton_2").addClass("active_chat");
             $("#chatButton2").show();
         } else {
             $("#chatButton_2").removeClass("active_chat");
             $("#chatButton2").hide();
         }
-        if (e.target.id == "chatButton_3") {
+        if (e.target.id == "chatButton_3" || $(e.target).parents("#chatButton_3").length) {
             $("#chatButton_3").addClass("active_chat");
             $("#chatButton3").show();
         } else {
             $("#chatButton_3").removeClass("active_chat");
             $("#chatButton3").hide();
         }
-        if (e.target.id == "chatButton_4") {
+        if (e.target.id == "chatButton_4" || $(e.target).parents("#chatButton_4").length) {
             $("#chatButton_4").addClass("active_chat");
             $("#chatButton4").show();
         } else {
@@ -68,15 +31,90 @@ $(document).ready(function () {
         }
     });
 
-    /* small conversation menu */
+  
     $(".otherOptions").click(function () {
         $(".moreMenu").slideToggle("fast");
     });
 
-    /* clicking the search button from the conversation focus the search bar outside it, as on desktop */
+   
     $(".search").click(function () {
         $(".searchChats").focus();
     });
+    
+    var btn = $("#btn");
+    $("#btn").click(function() {
+        $(".sidebar").toggleClass("active");
+    });
 
- 
+    //Channel notification icon
+    $("#bell").click(function() {
+        $(this).toggleClass("bx-bell-off");
+    });
+
+    //chatButton1 write panel
+    $('#panel_1').keypress(function(e) {
+        var message_1 =  e.target.value;
+        var dt = new Date();
+        console.log(message_1)
+
+        if(message_1) {
+
+            if(e.keyCode === 13) {
+                $("#chatPanel_1").append(`
+                    <div class="msg messageSent msg_2">
+				        ${message_1}
+				        <i class="material-icons readStatus">done</i>
+				        <span class="timestamp">${dt.getHours() + ":" + dt.getMinutes()}</span>
+			        </div>
+                `);
+
+                $("p[id='person_1']").attr("style", "display: none;");
+                $("#one").append(`
+                    <p class='message' id='person_1'>${message_1}</p>
+                `);
+
+                $("p[id='data_1']").attr("style", "display: none;");
+                $("#data1").append(`
+                    <p class="date" id="data_1">${dt.getHours() + ":" + dt.getMinutes()}</p>
+                `);
+
+                e.target.value = "";
+            }
+        }
+
+    });
+
+    //chatButton2 write panel
+    $('#panel_2').keypress(function(e) {
+        var message_2 =  e.target.value;
+        var dt = new Date();
+        console.log(message_2)
+
+        if(message_2) {
+
+            if(e.keyCode === 13) {
+                $("#chatPanel_2").append(`
+                    <div class="msg messageSent msg_2">
+				        ${message_2}
+				        <i class="material-icons readStatus">done</i>
+				        <span class="timestamp">${dt.getHours() + ":" + dt.getMinutes()}</span>
+			        </div>
+                `);
+
+                $("p[id='person_2']").attr("style", "display: none;")
+                $("#two").append(`
+                    <p class='message' id="person_2">${message_2}</p>
+                `);
+
+                $("p[id='data_2']").attr("style", "display: none;");
+                $("#data2").append(`
+                    <p class="date" id="data_2">${dt.getHours() + ":" + dt.getMinutes()}</p>
+                `);
+
+                e.target.value = "";
+            }
+        }
+
+    });
+  
 });
